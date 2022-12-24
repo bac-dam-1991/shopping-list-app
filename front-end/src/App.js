d7808b40b7564ae84d69f7b94a0bf791b981b72e
Bac Dam <bac.dam.1991@gmail.com>
2022-12-24 23:20:42 +1100

    Removed Title and Counter component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,3 @@
-import { Title } from "./components/Title";
-import { Counter } from "./components/Counter";
-
 export const App = () => {
-  return (
-    <div>
-      <Title title="Hello" subtitle={"World"} />
-      <Counter />
-    </div>
-  );
+  return <div>Shopping List Application</div>;
 };
