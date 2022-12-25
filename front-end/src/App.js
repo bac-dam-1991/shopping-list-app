54f67333e5f6dc0cf6e55e047eaf9945fb00347c
Bac Dam <bac.dam.1991@gmail.com>
2022-12-25 23:30:27 +1100

    Changed to Typescript and installed MUI.

--- front-end/src/App.js
+++
@@ -1,3 +0,0 @@
-export const App = () => {
-  return <div>Shopping List Application</div>;
-};
---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import { CssBaseline, Typography } from "@mui/material";
+
+export const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <Typography variant="h3" component={"h1"}>
+        Shopping List Application
+      </Typography>
+    </div>
+  );
+};
