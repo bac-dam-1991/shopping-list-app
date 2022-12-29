1c6632d61017126bc30653c01235e5cae718b6f4
Bac Dam <bac.dam.1991@gmail.com>
2022-12-29 21:48:01 +1100

    Successfully connected to Mongo.

---
+++ back-end/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const connectToMongo = async () => {
+  try {
+    const database = "shopping-list-app";
+    const scheme = "mongodb";
+    const host = "localhost";
+    const port = 27017;
+    const connectionString = `${scheme}://${host}:${port}`;
+    const client = new MongoClient(connectionString);
+    await client.connect();
+    const result = client.db(database);
+    return result;
+  } catch (error) {
+    console.error({
+      message: "Cannot create Mongo client",
+      description: (error as Error).message,
+    });
+    throw error;
+  }
+};
+
+app.get("/test", async (req, res) => {
+  const db = await connectToMongo();
+  const collection = db.collection("shopping-lists");
+  const cursor = await collection.find();
+  const docs = await cursor.toArray();
+  res.status(200).json(docs);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
